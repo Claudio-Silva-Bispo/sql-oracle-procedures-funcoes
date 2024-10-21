9b94c4b6ffa14bfca5776f7ae46c678c867f4426
Claudio Silva Bispo <110735259+Claudio-Silva-Bispo@users.noreply.github.com>
2024-10-20 21:26:48 -0300

    Update and rename 2. pandas-tratar-informacoes.ipynb to pandas-tratar-informacoes.py

--- Python/pandas-tratar-informacoes.py
+++ Python/pandas-tratar-informacoes.py
@@ -12,3 +12,6 @@ df_feedback_model.info()
 df_feedback_model['NOTA'] = df_feedback_model['NOTA'].astype(int)
 
 df_feedback_model
+
+# Salvar o DataFrame preparado em um arquivo CSV para uso posterior pelo professor
+df_feedback_model.to_csv('feedback_model_data.csv', index=False)
